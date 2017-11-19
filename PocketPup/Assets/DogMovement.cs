fe42e87e0748d126de00ecf9650ecb99ae9829e0
Nick <nick.crawford1234@gmail.com>
2017-11-18 20:08:35 -0500

    Dog walks and rotates in a square

--- PocketPup/Assets/DogMovement.cs
+++ PocketPup/Assets/DogMovement.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DogMovement : MonoBehaviour {
+	public Transform[] target;
+	public float speed;
+
+	private int current;
 
 	// Use this for initialization
 	void Start () {
-
+		Vector3 direction = target[current].position - transform.position;
+		Quaternion rotation = Quaternion.LookRotation (direction);
+		GetComponent<Rigidbody> ().MoveRotation (rotation);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(0f, 0f, 1f * Time.deltaTime);
+		if (transform.position != target[current].position)
+		{
+			Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed);
+			GetComponent<Rigidbody>().MovePosition(pos);
+		}
+		else
+		{
+
+			current = (current + 1) % target.Length;
+
+			Vector3 direction = target[current].position - transform.position;
+			Quaternion rotation = Quaternion.LookRotation (direction);
+			GetComponent<Rigidbody> ().MoveRotation (rotation);
+
+
+		
+		}
 	}
 }
